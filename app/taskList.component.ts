f87cf306e345a6f5dd522c150bf0b07e18614a24
gafilter <gafilter@gmail.com>
2017-01-05 14:16:08 +0100

    TaskList Binding Data From Component Class To Template

--- app/taskList.component.ts
+++ app/taskList.component.ts
@@ -9,24 +9,23 @@ import {Component} from 'angular2/core'
 /* print heading variable inside template */
 @Component ({
 	selector: 'task-list',
-	template: '<h1>{{heading}}</h1> <br/>Type Heading: <input type="text" #headingInput><button (click)="changeHeading(headingInput.value)">Change Heading</button>'
+	template: '<h1>{{heading1}}</h1> <h2>{{heading2}}</h2> <h3>{{heading3}}</h3>'
 })
+ 
+ 
  
 export class TaskListComponent{
  
 	/* define heading variables */
  
-	private heading;
+	private heading1
+	protected heading2
+	public heading3
  
 	constructor(){
-		this.heading = "Task List";
-	}
-
-	/* define method changeHeading */
-	/* Pass value to the method */
-	/* Assign passed value to heading property */
-	
-	changeHeading(value){
-		this.heading = value
+ 
+		this.heading1 = "Task List heading 1"
+		this.heading2 = "Task List heading 2"
+		this.heading3 = "Task List heading 3"
 	}
 }
