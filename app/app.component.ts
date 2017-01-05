bf177ca8f0c52d078f7ca1f6bb674e26115bb35c
gafilter <gafilter@gmail.com>
2017-01-05 13:22:47 +0100

    1-AngularJS 2 Getting Started. Hello world Application

---
+++ app/app.component.ts
@@ -0,0 +1,14 @@
+/* Importing Component decorator function from anularjs library module */
+
+import {Component} from 'angular2/core'
+
+
+/* Creating  AppComponent Class decorator */
+@Component ({
+	selector: 'learning-turn',
+	template: '<h1>AngularJS 2 Hello World Applicaiton: LearningTurn</h1>'
+})
+
+/* Creating AppComponent class */
+/* Exporting AppComponent Class */
+export class AppComponent{}
---
+++ app/main.ts
@@ -0,0 +1,9 @@
+/* importing bootstrap component from angular library */
+
+import {bootstrap} from 'angular2/platform/browser';
+
+/* Importing AppComponent created in app.component.ts file */
+import {AppComponent} from './app.component';
+
+/* Bootstrapping AppComponent */
+bootstrap(AppComponent);
